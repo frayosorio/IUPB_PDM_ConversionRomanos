7c523cbf126b9e541968a8f300794baaec2697c5
fray osorio <frayosorio@gmail.com>
2025-09-09 19:01:19 -0500

    Se crea interfaz grafica

---
+++ lib/vistas/conversor_vista.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ConversorVista extends StatefulWidget {
+  const ConversorVista({super.key});
+
+  @override
+  State<ConversorVista> createState() => _ConversorState();
+}
+
+class _ConversorState extends State<ConversorVista> {
+  
+}
