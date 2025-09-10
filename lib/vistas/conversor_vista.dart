e8fc49cba5f37e63d3a3bc088bef9a2cea726bf1
fray osorio <frayosorio@gmail.com>
2025-09-09 19:35:40 -0500

    Se crea la vista del formulario

--- lib/vistas/conversor_vista.dart
+++ lib/vistas/conversor_vista.dart
@@ -8,5 +8,38 @@ class ConversorVista extends StatefulWidget {
 }
 
 class _ConversorState extends State<ConversorVista> {
-  
+  final _txtNumero = TextEditingController();
+
+  @override
+  Widget build(BuildContext contexto) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Convertidor a Números Romanos"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          child: Column(
+            children: [
+              const Text(
+                "Digite el número a convertir:",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32),
+              TextFormField(
+                controller: _txtNumero,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: "Número arábigo (1-3999)",
+                  border: OutlineInputBorder()
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
