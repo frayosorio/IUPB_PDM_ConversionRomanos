21727f7733b9205b256bbb3c8ace5e3882ab1d6a
fray osorio <frayosorio@gmail.com>
2025-09-11 19:39:31 -0500

    Se completa la interfaz grafica y se programa validacion y evento de conversion a romano

--- lib/vistas/conversor_vista.dart
+++ lib/vistas/conversor_vista.dart
@@ -9,6 +9,55 @@ class ConversorVista extends StatefulWidget {
 
 class _ConversorState extends State<ConversorVista> {
   final _txtNumero = TextEditingController();
+  final _keyFormulario = GlobalKey<FormState>();
+  String _resultado = "Hola";
+
+  String _convertirARomano(int numero) {
+    if (numero <= 0 || numero > 3999) {
+      return "El número no está en el rango";
+    }
+    String romano = "";
+
+    const Map<int, String> digitosRomanos = {
+      1000: "M",
+      900: "CM",
+      500: "D",
+      400: "CD",
+      100: "C",
+      90: "XC",
+      50: "L",
+      40: "XL",
+      10: "X",
+      9: "IX",
+      5: "V",
+      4: "IV",
+      1: "I",
+    };
+
+    while (numero > 0) {
+      for (var clave in digitosRomanos.keys) {
+        if (numero > clave) {
+          romano += digitosRomanos[clave]!;
+          numero -= clave;
+          break;
+        }
+      }
+    }
+
+    return romano;
+  }
+
+  void _calcular() {
+    if (_keyFormulario.currentState!.validate()) {
+      final int? numero = int.tryParse(_txtNumero.text);
+      if (numero != null) {
+        final romano = _convertirARomano(numero);
+        setState(() {
+          _resultado = romano;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext contexto) {
@@ -20,6 +69,7 @@ class _ConversorState extends State<ConversorVista> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(24.0),
         child: Form(
+          key: _keyFormulario,
           child: Column(
             children: [
               const Text(
@@ -33,9 +83,26 @@ class _ConversorState extends State<ConversorVista> {
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
                   labelText: "Número arábigo (1-3999)",
-                  border: OutlineInputBorder()
-                )
+                  border: OutlineInputBorder(),
+                ),
+                validator: (valorDigitado) {
+                  if (valorDigitado == null || valorDigitado.isEmpty) {
+                    return "por favor ingrese un número";
+                  }
+                  final int? numero = int.tryParse(valorDigitado);
+                  if (numero == null || numero <= 0 || numero > 3999) {
+                    return "debe ingresar un número entre 1 y 3999";
+                  }
+                  return null;
+                },
               ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: _calcular,
+                child: const Text("Convertir"),
+              ),
+              const SizedBox(height: 32),
+              Text(_resultado, textAlign: TextAlign.center),
             ],
           ),
         ),
